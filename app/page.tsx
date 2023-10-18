c057a03b82b1d2a99fb65bd21f6ba621e7b8924c
Jon Dayton <jondayton@gmail.com>
2023-10-18 16:45:49 -0400

    set up tsdoc (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
  * @param {object} props
  * @returns {React.FC}
  */
-function HomePage(): JSX.Element {
+const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -120,4 +120,4 @@ function HomePage(): JSX.Element {
   )
 }
 
-export default HomePage;
+export default Home;
